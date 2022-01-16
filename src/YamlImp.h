2ef0c9d51f5bb70f310af8dadec41d18d28f8aee
taoyali <taoyali@taoyalis-iMac-Pro.local>
2022-01-17 00:23:06 +0800

    ref |> yaml-cpp拆分, 并屏蔽

---
+++ src/YamlImp.h
@@ -0,0 +1,19 @@
+
+// #include <yaml-cpp/yaml.h>
+
+// // std::string yaml = "/Users/taoyali/taoyali/project/C++/cmake/src/res/log.yml";
+
+// // 相对路径
+// // YAML::Node node = YAML::LoadFile(Config::yamlConfigFile);
+// YAML::Node node;
+
+// void yamlmap() {
+//     std::cout << node << std::endl;
+
+//     std::cout << "mainkey = " << node["key"]["mainkey"].as<std::string>()
+//               << std::endl;
+//     std::cout << "subkey = " << node["key"]["subkey"].as<std::string>() << std::endl;
+
+//     //std::cout << "name = " << node["logs"]["name"].as<std::string>() << std::endl;
+// }
+
--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,8 @@
 #include "src/AppConfigure.h"
 
 // #include <boost/boost/lexical_case.hpp>
-#include <yaml-cpp/yaml.h>
-
-// std::string yaml = "/Users/taoyali/taoyali/project/C++/cmake/src/res/log.yml";
 
 #include "config/filePathConfig.h"
-// 相对路径
-// YAML::Node node = YAML::LoadFile(Config::yamlConfigFile);
-YAML::Node node;
 
 struct Box
 {
@@ -40,15 +34,7 @@ struct Box
     int b;
 };
 
-void yamlmap() {
-    std::cout << node << std::endl;
 
-    std::cout << "mainkey = " << node["key"]["mainkey"].as<std::string>()
-              << std::endl;
-    std::cout << "subkey = " << node["key"]["subkey"].as<std::string>() << std::endl;
-
-    //std::cout << "name = " << node["logs"]["name"].as<std::string>() << std::endl;
-}
 
 void openFile() {
   char* filePath = "/res/resource.txt";
